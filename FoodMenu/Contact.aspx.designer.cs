1a960ce24768aa6e189f15241ae35aac9ef4e89a
Jorge Aguilar <jorge.bravo77@gmail.com>
2024-06-29 12:33:17 -0600

    Fix Contact Form

--- FoodMenu/Contact.aspx.designer.cs
+++ FoodMenu/Contact.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace FoodMenu
     {
 
         /// <summary>
-        /// imbDishImageFile control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.FileUpload imbDishImageFile;
-
-        /// <summary>
         /// Listbox1 control.
         /// </summary>
         /// <remarks>
@@ -40,6 +31,15 @@ namespace FoodMenu
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.CheckBox flexCheckDefault;
+
+        /// <summary>
+        /// imbDishImageFile control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload imbDishImageFile;
 
         /// <summary>
         /// MyButton control.
--- FoodMenu/Default.aspx
+++ FoodMenu/Default.aspx
@@ -10,7 +10,7 @@
                     <h5 class="text-white text-uppercase fw-bold mb-4" style="letter-spacing: 3px;">Comida Mexicana</h5>
                     <h1 class="display-1 text-capitalize text-white mb-4">Los mejores platillos regionales</h1>
                     <p class="mb-5 fs-5">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                     </p>
                     <a class="btn btn-primary rounded-pill text-white py-3 px-5" href="#">Book Appointment</a>
                 </div>
@@ -80,8 +80,6 @@
                     </div>
                 </div>
             </div>
-
-
         </div>
         <div class="container row" id="tacos">
             <h3>Tacos</h3>
